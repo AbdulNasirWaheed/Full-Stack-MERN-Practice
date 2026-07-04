377afeab76f58461bdd58bb78f6d0c8a0f07a494
AbdulNasirWaheed <AbdulNasirWaheed@users.noreply.github.com>
2026-07-05 00:56:38 +0500

    Add files via upload

---
+++ Week-06/Day-02/Task 1/script.js
@@ -0,0 +1,12 @@
+// Define the array
+const numbers = [10, 20, 30, 40, 50];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]; // Add each element to the sum variable
+}
+
+console.log("Sum = " + sum);
+
+document.getElementById("result-display").innerText = "Sum = " + sum;
